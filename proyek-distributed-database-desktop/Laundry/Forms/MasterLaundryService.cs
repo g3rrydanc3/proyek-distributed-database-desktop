7a91d2a3f8bd3a7459a0c9496b97d18aec7b8b88
Gerry Surya <ger_26_6@hotmail.com>
2018-12-18 20:45:12 +0700

    Load laundry service to datagridview

--- proyek-distributed-database-desktop/Laundry/Forms/MasterLaundryService.cs
+++ proyek-distributed-database-desktop/Laundry/Forms/MasterLaundryService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,31 @@ namespace proyek_distributed_database_desktop.Laundry
 {
     public partial class MasterLaundryService : Form
     {
+        OracleConnection conn;
         public MasterLaundryService()
         {
             InitializeComponent();
+        }
+
+        private void MasterLaundryService_Load(object sender, EventArgs e)
+        {
+            conn = new OracleConnection(Login.connectionString);
+            conn.Open();
+            OracleCommand cmd = new OracleCommand("select * from laundry_service", conn);
+            OracleDataReader reader = cmd.ExecuteReader();
+            DataTable dataTable = new DataTable();
+            dataTable.Load(reader);
+            
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells[2] != null)
+                {
+                    //row.Cells[2].Value = Rupiah.ToRupiah(Int32.Parse(row.Cells[2].Value.ToString()));
+                }
+            }
+
+            dataGridView1.DataSource = dataTable;
         }
     }
 }
