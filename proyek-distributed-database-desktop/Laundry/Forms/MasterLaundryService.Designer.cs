9ee2d92fc432bb19f6564b86a12590acc8b6bd9c
Gerry Surya <ger_26_6@hotmail.com>
2018-12-18 23:47:08 +0700

    Read data to form

--- proyek-distributed-database-desktop/Laundry/Forms/MasterLaundryService.Designer.cs
+++ proyek-distributed-database-desktop/Laundry/Forms/MasterLaundryService.Designer.cs
@@ -29,36 +29,210 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonRevert = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonAddEdit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.textBoxServiceType = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDownPrice = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPrice)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(17, 16);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeight = 28;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 14);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(320, 185);
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.ShowCellErrors = false;
+            this.dataGridView1.ShowCellToolTips = false;
+            this.dataGridView1.ShowEditingIcon = false;
+            this.dataGridView1.ShowRowErrors = false;
+            this.dataGridView1.Size = new System.Drawing.Size(292, 308);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Enabled = false;
+            this.buttonSave.Location = new System.Drawing.Point(193, 332);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(112, 37);
+            this.buttonSave.TabIndex = 1;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonRevert
+            // 
+            this.buttonRevert.Enabled = false;
+            this.buttonRevert.Location = new System.Drawing.Point(13, 332);
+            this.buttonRevert.Name = "buttonRevert";
+            this.buttonRevert.Size = new System.Drawing.Size(91, 37);
+            this.buttonRevert.TabIndex = 2;
+            this.buttonRevert.Text = "Revert";
+            this.buttonRevert.UseVisualStyleBackColor = true;
+            this.buttonRevert.Click += new System.EventHandler(this.buttonRevert_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonAddEdit, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonCancel, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxServiceType, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.numericUpDownPrice, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 25);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(285, 118);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Service Type";
+            // 
+            // buttonAddEdit
+            // 
+            this.buttonAddEdit.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonAddEdit.Location = new System.Drawing.Point(145, 81);
+            this.buttonAddEdit.Name = "buttonAddEdit";
+            this.buttonAddEdit.Size = new System.Drawing.Size(137, 34);
+            this.buttonAddEdit.TabIndex = 5;
+            this.buttonAddEdit.Text = "Add";
+            this.buttonAddEdit.UseVisualStyleBackColor = true;
+            this.buttonAddEdit.Click += new System.EventHandler(this.buttonAddEdit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 39);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Price";
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonCancel.Location = new System.Drawing.Point(3, 81);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(136, 34);
+            this.buttonCancel.TabIndex = 4;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // textBoxServiceType
+            // 
+            this.textBoxServiceType.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxServiceType.Location = new System.Drawing.Point(145, 3);
+            this.textBoxServiceType.Name = "textBoxServiceType";
+            this.textBoxServiceType.Size = new System.Drawing.Size(137, 29);
+            this.textBoxServiceType.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tableLayoutPanel1);
+            this.groupBox1.Location = new System.Drawing.Point(312, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(291, 146);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add";
+            // 
+            // numericUpDownPrice
+            // 
+            this.numericUpDownPrice.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.numericUpDownPrice.Location = new System.Drawing.Point(145, 42);
+            this.numericUpDownPrice.Maximum = new decimal(new int[] {
+            1215752191,
+            23,
+            0,
+            0});
+            this.numericUpDownPrice.Name = "numericUpDownPrice";
+            this.numericUpDownPrice.Size = new System.Drawing.Size(137, 29);
+            this.numericUpDownPrice.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(311, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Double Click to Edit";
             // 
             // MasterLaundryService
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(385, 242);
+            this.ClientSize = new System.Drawing.Size(616, 381);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonRevert);
+            this.Controls.Add(this.buttonSave);
             this.Controls.Add(this.dataGridView1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "MasterLaundryService";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Master Laundry Service";
             this.Load += new System.EventHandler(this.MasterLaundryService_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPrice)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonRevert;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonAddEdit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.TextBox textBoxServiceType;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDownPrice;
+        private System.Windows.Forms.Label label3;
     }
 }
--- proyek-distributed-database-desktop/Laundry/Forms/MasterLaundryService.cs
+++ proyek-distributed-database-desktop/Laundry/Forms/MasterLaundryService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,10 @@ namespace proyek_distributed_database_desktop.Laundry
     public partial class MasterLaundryService : Form
     {
         OracleConnection conn;
+        OracleCommand cmd;
+        OracleTransaction trans;
+        int id_clicked = -1;
+
         public MasterLaundryService()
         {
             InitializeComponent();
@@ -23,21 +28,79 @@ namespace proyek_distributed_database_desktop.Laundry
         {
             conn = new OracleConnection(Login.connectionString);
             conn.Open();
+            refresh();
+        }
+
+        private void refresh()
+        {
             OracleCommand cmd = new OracleCommand("select * from laundry_service", conn);
             OracleDataReader reader = cmd.ExecuteReader();
             DataTable dataTable = new DataTable();
             dataTable.Load(reader);
-            
-
-            foreach (DataGridViewRow row in dataGridView1.Rows)
-            {
-                if (row.Cells[2] != null)
-                {
-                    //row.Cells[2].Value = Rupiah.ToRupiah(Int32.Parse(row.Cells[2].Value.ToString()));
-                }
-            }
 
             dataGridView1.DataSource = dataTable;
+            if (dataGridView1.Columns.Count >= 2)
+            {
+                dataGridView1.Columns[2].DefaultCellStyle.Format = "c2";
+                dataGridView1.Columns[2].DefaultCellStyle.FormatProvider = CultureInfo.GetCultureInfo("id-ID");
+            }
+        }
+
+        private void resetForm()
+        {
+            id_clicked = -1;
+            buttonAddEdit.Text = "Add";
+            buttonCancel.Enabled = false;
+            textBoxServiceType.Text = "";
+            numericUpDownPrice.Value = 0;
+            groupBox1.Text = "Add";
+        }
+
+        private void reset()
+        {
+            resetForm();
+            buttonRevert.Enabled = false;
+            buttonSave.Enabled = false;
+        }
+        
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex != -1)
+            {
+                id_clicked = Int32.Parse(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+                buttonCancel.Enabled = true;
+                buttonAddEdit.Text = "Edit";
+                groupBox1.Text = "Edit";
+
+                OracleCommand cmd = new OracleCommand("select service_type, price from laundry_service where laundry_service_id = :laundry_service_id", conn);
+                cmd.Parameters.Add("laundry_service_id", id_clicked);
+                OracleDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    textBoxServiceType.Text = reader.GetString(0);
+                    numericUpDownPrice.Value = reader.GetInt32(1);
+                }
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            resetForm();
+        }
+
+        private void buttonAddEdit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRevert_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
