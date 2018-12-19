388637e2463edf2d7850e3c804ee6eb99337a792
Gerry Surya <ger_26_6@hotmail.com>
2018-12-19 18:05:35 +0700

    Laundry new done

--- proyek-distributed-database-desktop/Laundry/Forms/NewLaundry.Designer.cs
+++ proyek-distributed-database-desktop/Laundry/Forms/NewLaundry.Designer.cs
@@ -29,76 +29,271 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Service = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Weight = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Total = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDownPrice = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownWeight = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBoxWalkIn = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelTotal = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPrice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWeight)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(338, 14);
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeight = 35;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Service,
+            this.Weight,
+            this.Price,
+            this.Total});
+            this.dataGridView1.Location = new System.Drawing.Point(306, 14);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(258, 223);
+            this.dataGridView1.Size = new System.Drawing.Size(508, 332);
             this.dataGridView1.TabIndex = 0;
             // 
-            // groupBox1
+            // Service
             // 
-            this.groupBox1.Location = new System.Drawing.Point(18, 19);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBox1.Size = new System.Drawing.Size(300, 162);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.Service.HeaderText = "Service";
+            this.Service.Name = "Service";
+            // 
+            // Weight
+            // 
+            this.Weight.HeaderText = "Weight";
+            this.Weight.Name = "Weight";
+            // 
+            // Price
+            // 
+            this.Price.HeaderText = "Price";
+            this.Price.Name = "Price";
+            // 
+            // Total
+            // 
+            this.Total.HeaderText = "Total";
+            this.Total.Name = "Total";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 28;
+            this.listBox1.Location = new System.Drawing.Point(12, 52);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(286, 200);
+            this.listBox1.TabIndex = 0;
             // 
             // groupBox2
             // 
-            this.groupBox2.Location = new System.Drawing.Point(18, 192);
+            this.groupBox2.Controls.Add(this.numericUpDownPrice);
+            this.groupBox2.Controls.Add(this.numericUpDownWeight);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.buttonAdd);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 260);
             this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBox2.Size = new System.Drawing.Size(300, 162);
+            this.groupBox2.Size = new System.Drawing.Size(286, 214);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "groupBox2";
             // 
-            // button1
+            // numericUpDownPrice
             // 
-            this.button1.Location = new System.Drawing.Point(338, 245);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 30);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.numericUpDownPrice.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDownPrice.Location = new System.Drawing.Point(89, 118);
+            this.numericUpDownPrice.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.numericUpDownPrice.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDownPrice.Name = "numericUpDownPrice";
+            this.numericUpDownPrice.Size = new System.Drawing.Size(190, 34);
+            this.numericUpDownPrice.TabIndex = 4;
+            this.numericUpDownPrice.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDownWeight
+            // 
+            this.numericUpDownWeight.Location = new System.Drawing.Point(89, 78);
+            this.numericUpDownWeight.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownWeight.Name = "numericUpDownWeight";
+            this.numericUpDownWeight.Size = new System.Drawing.Size(190, 34);
+            this.numericUpDownWeight.TabIndex = 3;
+            this.numericUpDownWeight.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 28);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Price";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Weight";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(160, 158);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(119, 41);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(7, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(272, 36);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // checkBoxWalkIn
+            // 
+            this.checkBoxWalkIn.AutoSize = true;
+            this.checkBoxWalkIn.Location = new System.Drawing.Point(12, 14);
+            this.checkBoxWalkIn.Name = "checkBoxWalkIn";
+            this.checkBoxWalkIn.Size = new System.Drawing.Size(187, 32);
+            this.checkBoxWalkIn.TabIndex = 1;
+            this.checkBoxWalkIn.Text = "Walk in Customer";
+            this.checkBoxWalkIn.UseVisualStyleBackColor = true;
+            this.checkBoxWalkIn.CheckedChanged += new System.EventHandler(this.checkBoxWalkIn_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(455, 351);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 28);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Total";
+            // 
+            // labelTotal
+            // 
+            this.labelTotal.AutoEllipsis = true;
+            this.labelTotal.Location = new System.Drawing.Point(515, 351);
+            this.labelTotal.Name = "labelTotal";
+            this.labelTotal.Size = new System.Drawing.Size(300, 28);
+            this.labelTotal.TabIndex = 5;
+            this.labelTotal.Text = "Rp 0.00";
+            this.labelTotal.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(712, 409);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(103, 41);
+            this.buttonSave.TabIndex = 6;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(306, 409);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(109, 41);
+            this.buttonCancel.TabIndex = 7;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
             // 
             // NewLaundry
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(607, 371);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(827, 512);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.labelTotal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.checkBoxWalkIn);
             this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.dataGridView1);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "NewLaundry";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "New Laundry";
+            this.Load += new System.EventHandler(this.NewLaundry_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPrice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWeight)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBoxWalkIn;
+        private System.Windows.Forms.NumericUpDown numericUpDownPrice;
+        private System.Windows.Forms.NumericUpDown numericUpDownWeight;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelTotal;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Service;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Weight;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Total;
     }
 }
--- proyek-distributed-database-desktop/Laundry/Forms/NewLaundry.cs
+++ proyek-distributed-database-desktop/Laundry/Forms/NewLaundry.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,163 @@ namespace proyek_distributed_database_desktop.Laundry
 {
     public partial class NewLaundry : Form
     {
+        OracleConnection conn;
+        Dictionary<String, int> serviceTypes = new Dictionary<String, int>();
+
         public NewLaundry()
         {
             InitializeComponent();
+        }
+
+        private void NewLaundry_Load(object sender, EventArgs e)
+        {
+            conn = new OracleConnection(Login.connectionString);
+            conn.Open();
+            refresh();
+            reset();
+
+            dataGridView1.Columns[2].DefaultCellStyle.Format = "c2";
+            dataGridView1.Columns[2].DefaultCellStyle.FormatProvider = CultureInfo.GetCultureInfo("id-ID");
+            dataGridView1.Columns[3].DefaultCellStyle.Format = "c2";
+            dataGridView1.Columns[3].DefaultCellStyle.FormatProvider = CultureInfo.GetCultureInfo("id-ID");
+        }
+
+        private void reset()
+        {
+            checkBoxWalkIn.Checked = false;
+            dataGridView1.Rows.Clear();
+            listBox1.SelectedIndex = 0;
+            comboBox1.SelectedIndex = 0;
+            labelTotal.Text = "Rp 0.00";
+        }
+
+        private void refresh()
+        {
+            OracleCommand cmd = new OracleCommand(@"
+                select room.room_no, room_type.room_type 
+                from room@keFrontOffice, room_type@keFrontOffice
+                where room_type.room_type_id = room.type_id and 
+                room.status=0", conn);
+
+            OracleDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                listBox1.Items.Add(reader.GetInt32(0) + " - " + reader.GetString(1));
+            }
+            cmd.CommandText = "select * from laundry_service";
+            reader = cmd.ExecuteReader();
+            List<String> serviceTypesList = new List<string>();
+            while (reader.Read())
+            {
+                serviceTypesList.Add(String.Format("{0} - {1}", reader.GetString(1), Rupiah.ToRupiah(reader.GetInt32(2))));
+                serviceTypes.Add(reader.GetString(1), reader.GetInt32(0));
+            }
+            comboBox1.Items.AddRange(serviceTypesList.ToArray());
+
+            if (listBox1.SelectedIndex == -1) listBox1.SelectedIndex = 0;
+            if (comboBox1.SelectedIndex == -1) comboBox1.SelectedIndex = 0;
+        }
+
+        private void checkBoxWalkIn_CheckedChanged(object sender, EventArgs e)
+        {
+            listBox1.Enabled = !checkBoxWalkIn.Checked;
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            String service_type = comboBox1.SelectedItem.ToString().Split('-')[0].Trim();
+            bool ada = false;
+            int total = 0;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells[0].Value.ToString() == service_type)
+                {
+                    row.Cells[1].Value = Int32.Parse(row.Cells[1].Value.ToString()) + numericUpDownWeight.Value;
+                    row.Cells[2].Value = numericUpDownPrice.Value;
+                    row.Cells[3].Value = Int32.Parse(row.Cells[1].Value.ToString()) * Int32.Parse(row.Cells[2].Value.ToString());
+                    ada = true;
+                    break;
+                }
+            }
+
+            if (!ada)
+            {
+                dataGridView1.Rows.Add(service_type, numericUpDownWeight.Value, numericUpDownPrice.Value, numericUpDownPrice.Value * numericUpDownWeight.Value);
+            }
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                total += Int32.Parse(row.Cells[3].Value.ToString());
+            }
+
+            labelTotal.Text = Rupiah.ToRupiah(total);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            numericUpDownPrice.Value = Rupiah.ToAngka(comboBox1.SelectedItem.ToString().Split('-')[1].Trim());
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            String laundry_bill_id = insertLaundry();
+            MessageBox.Show(laundry_bill_id.ToString());
+            insertLaundryDetail(laundry_bill_id);
+            reset();
+        }
+
+        private String insertLaundry()
+        {
+            DateTime now = DateTime.Now;
+
+            OracleParameter dateParam = new OracleParameter();
+            dateParam.OracleDbType = OracleDbType.Date;
+            dateParam.Value = now;
+
+
+            OracleCommand cmd = conn.CreateCommand();
+
+            cmd.CommandText = "insert into laundry_bill (room_no, employee_id, total, bill_date) values(:a1, :a2, :a3, :a4)";
+            if (checkBoxWalkIn.Checked)
+            {
+                cmd.Parameters.Add("a1", "0");
+            }
+            else
+            {
+                cmd.Parameters.Add("a1", Int32.Parse(listBox1.SelectedItem.ToString().Split('-')[0].Trim()));
+            }
+            cmd.Parameters.Add("a2", "DUMMY");
+            cmd.Parameters.Add("a3", Rupiah.ToAngka(labelTotal.Text));
+            cmd.Parameters.Add(dateParam);
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = "select max(substr(laundry_bill_id,7)) from laundry_bill";
+            OracleDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                return now.ToString("ddMMyy") + reader.GetString(0);
+            }
+            throw new Exception("Laundry Bill not inserted");
+        }
+
+        private void insertLaundryDetail(String laundry_bill_id)
+        {
+            OracleCommand cmd = conn.CreateCommand();
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                cmd.Parameters.Clear();
+                cmd.CommandText = "insert into laundry_bill_detail (laundry_bill_id, laundry_service_id, weight, price) values(:a1, :a2, :a3, :a4)";
+                cmd.Parameters.Add("a1", laundry_bill_id);
+                cmd.Parameters.Add("a2", serviceTypes[row.Cells[0].Value.ToString()]);
+                cmd.Parameters.Add("a3", row.Cells[1].Value.ToString());
+                cmd.Parameters.Add("a4", row.Cells[2].Value.ToString());
+                cmd.ExecuteNonQuery();
+            }
         }
     }
 }
